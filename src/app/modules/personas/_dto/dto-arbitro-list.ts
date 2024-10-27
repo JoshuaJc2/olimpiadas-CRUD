8066b83106d6067e5a383c2d2dc11c7824c57b8a
JoshuaJc2 <Joshua-J-C@ciencias.unam.mx>
2024-10-27 12:23:20 -0600

    Archivos inutiles

--- src/app/modules/personas/_dto/dto-arbitro-list.ts
+++
@@ -1,6 +0,0 @@
-export class DtoArbitroList{
-    idarbitro : number = 0;
-    nombre : String = "";
-    primer_apellido : String = "";
-    segundo_apellido : String = "";
-}
--- src/app/modules/personas/_dto/dto-entrenador-list.ts
+++
@@ -1,6 +0,0 @@
-export class DtoEntrenadorList{
-    idarbitro : number = 0;
-    nombre : String = "";
-    primer_apellido : String = "";
-    segundo_apellido : String = "";
-}
